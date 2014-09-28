98afd91431ac5f0f25c1d5a8cd87f71de0219bc5
minestrenght <minestrenght@gmail.com>
2014-09-28 15:51:00 +0530

    demo of deffie halman

---
+++ shrunked/Deffie/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+	public static void main(String[] args) {
+		double p1 = 3.0, p2 = 7.0;//prime nos.
+		double x = 6.0, y = 5.0;//symmetric values
+		double s1 = Math.pow(p1, x) % p2;
+		double s2 = Math.pow(p1, y) % p2;
+		s2 = Math.pow(s2, x) % p2;
+		s1 = Math.pow(s1, y) % p2;
+		System.out.printf("Shared symmetric key test: %s\n", (s1 == s2 ? "Pass" : "Fail"));
+	}
+}
